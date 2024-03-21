8132ff326882ae9004e8fb4318db0ca928ca775e
Dilpreet Gill <gillds@dukes.jmu.edu>
2024-03-21 10:53:36 -0400

    docstrimngs

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 '''Michael Jolley and Dilpreet Gill - Lab 03 - 4.4.1 and 4.4.2'''
 
 def presentMatrixOptions():
+    """
+    Presents user to choose mode of operation for the matrix.
+
+    Parameters:
+
+    Returns:
+    selection (int): User's selected choice for the mode of operation for the matrix.
+    """
     # 4.4.1 Menu Creation
     print("1. Add matrices")
     print("2. Subtract matrices")
@@ -8,14 +16,33 @@ def presentMatrixOptions():
     print("4. Multiply matrices by a scalar")
 
     selection = int(input("Enter your selection: "))
+    return selection
 
 def getDimensions():
+    """
+    Generates dimensions for a matrix.
+
+    Parameters:
+
+    Returns:
+    rows (int): Dimensions of the rows of the matrix.
+    columns (int): Dimensions of the columns of the matrix.
+    """
     rows = int(input("Enter the number of rows: "))
     columns = int(input("Enter the number of columns: "))
     return rows, columns
 
-def getMatrix(dimensions):
-    rows, columns = dimensions
+def getMatrix(rows, columns):
+    """
+    Creates a matrix with numbers provided by the user.
+
+    Parameters:
+    rows (int): Dimensions of the rows of the matrix.
+    columns (int): Dimensions of the columns of the matrix.
+
+    Returns:
+    Matrix (list): Matrix
+    """
     matrix = []
     for i in range(rows):
         matrix.append([])
@@ -24,6 +51,17 @@ def getMatrix(dimensions):
     return matrix
 
 def validateMatrices(mat1, mat2, operation):
+    """
+    Determines if, given mode of operation, if it is possible between matricies.
+
+    Parameters:
+    mat1 (smth)
+    mat2 (smth)
+    operation (smth)
+
+    Returns:
+    bool (idk its true or false)
+    """
     if operation == 'add' or operation == 'subtract':
         return len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0])
     elif operation == 'multiply':
